bc1d4ad1dcd645d43f965ca28931a37fd332f8ff
Zach Bimson <zach.bimson@gmail.com>
2023-03-26 15:26:58 +0100

    MORE TESTSSS

--- tests/test_long_to_short_cli.py
+++ tests/test_long_to_short_cli.py
@@ -1,14 +1,20 @@
 import unittest
+from unittest.mock import patch
 import os
 from moviepy.editor import VideoFileClip, ColorClip, concatenate_videoclips
-from long_to_short_cli import process_video, time_str_to_seconds
+from long_to_short_cli import process_video, time_str_to_seconds, crop_to_aspect_ratio, main
 
-def generate_test_video(output_file, size=(1280, 720)):
-    clip1 = ColorClip(size=size, color=(255, 0, 0)).set_duration(3)
-    clip2 = ColorClip(size=size, color=(0, 255, 0)).set_duration(3)
-    clip3 = ColorClip(size=size, color=(0, 0, 255)).set_duration(3)
+def generate_test_video(output_file, size=(1280, 720), aspect_ratio=(9, 16)):
+    width, height = size
+    clip1 = ColorClip(size=(width, height), color=(255, 0, 0)).set_duration(3)
+    clip2 = ColorClip(size=(width, height), color=(0, 255, 0)).set_duration(3)
+    clip3 = ColorClip(size=(width, height), color=(0, 0, 255)).set_duration(3)
 
     final_clip = concatenate_videoclips([clip1, clip2, clip3])
+
+    if aspect_ratio:
+        final_clip = crop_to_aspect_ratio(final_clip, aspect_ratio[0] / aspect_ratio[1])
+
     final_clip.write_videofile(output_file, fps=24)
 
 class TestLongToShortCLI(unittest.TestCase):
@@ -91,6 +97,33 @@ class TestLongToShortCLI(unittest.TestCase):
 
         os.remove(input_file)
         os.remove(output_file)
+
+    def test_process_video_wider_aspect_ratio(self):
+        # Use a video with a 16:9 aspect ratio
+        generate_test_video(self.input_file, aspect_ratio=(16, 9))
+        process_video(self.input_file, self.output_file, self.start_time, self.end_time)
+
+        self.assertTrue(os.path.exists(self.output_file), "Output file not created")
+        output_clip = VideoFileClip(self.output_file)
+        self.assertAlmostEqual(output_clip.duration, 5, delta=0.1, msg="Output clip duration is incorrect")
+        self.assertAlmostEqual(output_clip.aspect_ratio, 9 / 16, delta=0.01, msg="Output clip aspect ratio is incorrect")
+
+    def test_invalid_start_time(self):
+        with self.assertRaises(ValueError):
+            process_video(self.input_file, self.output_file, "invalid_time", self.end_time)
+
+    def test_invalid_end_time(self):
+        with self.assertRaises(ValueError):
+            process_video(self.input_file, self.output_file, self.start_time, "invalid_time")
+
+    def test_main(self):
+        with patch("sys.argv", ["long_to_short_cli.py", self.input_file, self.output_file, self.start_time, self.end_time]):
+            main()
+
+        self.assertTrue(os.path.exists(self.output_file), "Output file not created")
+        output_clip = VideoFileClip(self.output_file)
+        self.assertAlmostEqual(output_clip.duration, 5, delta=0.1, msg="Output clip duration is incorrect")
+        self.assertAlmostEqual(output_clip.aspect_ratio, 9 / 16, delta=0.01, msg="Output clip aspect ratio is incorrect")
 
 if __name__ == '__main__':
     unittest.main()
