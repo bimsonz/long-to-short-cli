2277759cc86d7f17df22341ea20a3242c051089b
Zach Bimson <zach.bimson@gmail.com>
2023-03-26 13:20:02 +0100

    Updated time parameters to take HH:MM:SS format rather than seconds

--- long_to_short_cli.py
+++ long_to_short_cli.py
@@ -1,6 +1,15 @@
 import argparse
 from moviepy.editor import VideoFileClip
 
+def time_str_to_seconds(time_str):
+    time_parts = list(map(int, time_str.split(':')))
+    seconds = 0
+
+    for part in time_parts:
+        seconds = seconds * 60 + part
+
+    return seconds
+
 def crop_to_aspect_ratio(clip, aspect_ratio):
     width, height = clip.size
     new_width = int(height * aspect_ratio)
@@ -16,23 +25,24 @@ def crop_to_aspect_ratio(clip, aspect_ratio):
 def process_video(input_video, output_video, start_time, end_time):
     video = VideoFileClip(input_video)
 
-    selected_clip = video.subclip(start_time, end_time)
+    start_time_seconds = time_str_to_seconds(start_time)
+    end_time_seconds = time_str_to_seconds(end_time)
+
+    selected_clip = video.subclip(start_time_seconds, end_time_seconds)
 
     aspect_ratio = 9 / 16
     cropped_video = crop_to_aspect_ratio(selected_clip, aspect_ratio)
 
-    # Preserve the audio from the original video
     cropped_video = cropped_video.set_audio(selected_clip.audio)
 
-    # Write the output video file with specified codecs, bitrates, and threads
     cropped_video.write_videofile(output_video, codec='libx264', bitrate='5000k', audio_codec='aac', audio_bitrate='320k', threads=4)
 
 def main():
     parser = argparse.ArgumentParser(description='Extract a clip using start and end times and crop to 9:16 aspect ratio using MoviePy')
     parser.add_argument('input_video', type=str, help='Path to the input video file')
     parser.add_argument('output_video', type=str, help='Path to the output video file')
-    parser.add_argument('start_time', type=float, help='Start time of the clip (in seconds)')
-    parser.add_argument('end_time', type=float, help='End time of the clip (in seconds)')
+    parser.add_argument('start_time', type=str, help='Start time of the clip (in format HH:MM:SS, MM:SS, or SS)')
+    parser.add_argument('end_time', type=str, help='End time of the clip (in format HH:MM:SS, MM:SS, or SS)')
 
     args = parser.parse_args()
 
